8d324c248e049390412a3c276cbf84ac6fd1e54d
Baoming520 <dxl926@gmail.com>
2021-03-02 13:06:59 +0800

    update the code to adapt the update by Kingsoft

--- DownloadDataApp/main.py
+++ DownloadDataApp/main.py
@@ -49,6 +49,7 @@ def job():
         # Config with headless mode
         chrome_options = Options()
         chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
         wd = webdriver.Chrome(web_driver_path, chrome_options=chrome_options)
     else:
         wd = webdriver.Chrome(web_driver_path)
@@ -56,9 +57,17 @@ def job():
     try:
         wd.get(config['website'])
 
+        # Handle the stopping-sharing dialog (1/2)
+        # (CAUTION: I have no idea why it should be added here, and maybe it will be removed later!)
+        stop_shared_btn = wd.find_element_by_xpath('//button[@class="el-button btn-action el-button--primary"]')
+        if stop_shared_btn:
+            stop_shared_btn.click()
+            wd.implicitly_wait(config['loading_timeout'])
+
         # Confirmation: Invite to cooperate
-        wd.find_element_by_xpath(
-            '//div[@class="preview-error-container coopInvite"]/a/button[@class="el-button btn btn-allow el-button--primary"]').click()
+        # (CAUTION: This check has been closed now [2021/3/1].)
+        # wd.find_element_by_xpath(
+        #     '//div[@class="preview-error-container coopInvite"]/a/button[@class="el-button btn btn-allow el-button--primary"]').click()
 
         # Wait for loading the login options page
         # Set the timeout for loading page
@@ -101,8 +110,36 @@ def job():
         # Click the login button
         loginBtn = wd.find_element_by_id('login')
         # loginBtn.click()  # Go to the personal info page
-        wd.execute_script('arguments[0].click();', loginBtn)
-        print('INFO: 返回数据页面')
+        # wd.execute_script('arguments[0].click();', loginBtn) # Execute script directly
+        js_code = 'document.getElementById("login").click();'
+        wd.execute_script(js_code)
+
+        # Handle the stopping-sharing dialog (2/2)
+        # (CAUTION: I have no idea why it should be added here, and maybe it will be removed later!)
+        stop_shared_btn = wd.find_element_by_xpath('//button[@class="el-button btn-action el-button--primary"]')
+        if stop_shared_btn:
+            stop_shared_btn.click()
+            wd.implicitly_wait(config['loading_timeout'])
+
+        # Process all the doc-list in the page
+        fFlag = False
+        doc_list = wd.find_elements_by_xpath('//div[@class="yun-list__item" or @class="yun-list__item yun-list__item--selected"]')
+        if doc_list:
+            for doc in doc_list:
+                if doc.text.replace('\n', '').find(config['yun_item_name']) != -1:
+                    fFlag = True
+                    doc_name = doc.find_element_by_xpath('./div[@class="yun-list__inner"]/div[@class="yun-row mouse-context-item"]/div[@class="yun-row__cell yun-row__meta"]/div/div[@class="yun-row__name"]')
+                    doc_name.click()
+                    wd.implicitly_wait(config['loading_timeout'])
+                    print(wd.window_handles)
+                    if(len(wd.window_handles) > 1):
+                        # A new page will be openned, and jump to the new page
+                        wd.switch_to_window(wd.window_handles[1])
+                    break
+        
+        if not fFlag:
+            print('The specified file is not found in the current page.')
+            return
 
         # Wait for the data page is loaded completely
         print('[START]: Waiting for the specified element')
@@ -110,7 +147,7 @@ def job():
         WDWait(wd, 20).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.component-icon-btn.header-more-btn'))) # The class name has more than one values
         print('[END]: Found the element')
 
-        time.sleep(config['normal_timeout'] + 5)
+        print('INFO: 返回数据页面')
         
         menu_btn = wd.find_element_by_xpath(
             '//div[@class="component-icon-btn header-more-btn"]')
